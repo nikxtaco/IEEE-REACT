f25456e40ed9a1e01ac2cb95d371409bb80c1cbc
nikxtaco <nikitamenon2510@gmail.com>
2020-03-14 22:45:08 +0530

    adding navbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar.js'
 import Carousel from './Carousel.js'
+import "../css/landing.css"
 
 function Landing() {
   return (
+    <body>
     <div id="landing">
       
     <div className="navbar"><Navbar/></div>
     <div className="carousel"><Carousel/></div>
 
     </div>
+    </body>
   );
 }
 
