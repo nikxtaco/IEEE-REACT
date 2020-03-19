0c0d7b54446a51000da15d9f975450105a1e9723
unknown <nikitamenon2510@gmail.com>
2020-03-19 14:17:15 +0530

    adding basic gallery and footer

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar.js'
 import Carousel from './Carousel.js'
+import Photos from './Gallery.js'
+import Footer from './Footer.js'
 import "../css/landing.css"
 
 function Landing() {
@@ -10,6 +12,8 @@ function Landing() {
       
     <div className="navbar"><Navbar/></div>
     <div className="carousel"><Carousel/></div>
+    <div className="gallery"><Photos/></div>
+    <div className="footer"><Footer/></div>
 
     </div>
     </body>
