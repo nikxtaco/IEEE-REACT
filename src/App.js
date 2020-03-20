bfb2230ba485c4da4a3502b158579952620f31a8
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-20 16:40:00 +0000

    changed nav to react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Landing from './components/Landing.js'
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Navbar from './Navbar.js'
-import Carousel from './Carousel.js'
-import Photos from './Gallery.js'
-import Footer from './Footer.js'
-import "../css/landing.css"
+import Carousel from './Carousel.js';
+import Navigation_bar from './Navigation_bar.js';
+import Photos from './Gallery.js';
+import Footer from './Footer.js';
+import "../css/landing.css";
+
 
 function Landing() {
   return (
     <body>
     <div id="landing">
       
-    <div className="navbar"><Navbar/></div>
+    <div className="navbar"><Navigation_bar/></div>
     <div className="carousel"><Carousel/></div>
     <div className="gallery"><Photos/></div>
     <div className="footer"><Footer/></div>
