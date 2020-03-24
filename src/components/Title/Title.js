1c1fb91121dd27f9a0a49c87268d73088d158075
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-24 18:49:30 +0000

    added jumbotron

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,14 +1,20 @@
 import React from  'react';
 import './Title.css'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 
 const title = props =>{
     return(
         <div className="title">
-            <div className="container"></div>
-            <span className="title_content">
-
-            </span>
+            <Jumbotron fluid className="jumbo">
+                <Container>
+                    <h1 id="heading">Fluid jumbotron</h1>
+                    <p id="para">
+                        This is a modified jumbotron that occupies the entire horizontal space of
+                        its parent.
+                    </p>
+                </Container>
+            </Jumbotron>
         </div>
     )
 }
