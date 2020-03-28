49828555777a4aa5684efaceba7f7e10491ad10d
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-28 16:34:49 +0000

    added title.js nad title.css

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -6,16 +6,19 @@ import Container from 'react-bootstrap/Container'
 const title = props =>{
     return(
         <div className="title">
-            <Jumbotron fluid className="jumbo">
-                <Container>
-                    <h1 id="heading">Fluid jumbotron</h1>
+            <Jumbotron fluid className="jumbo" style={{backgroundColor:"white"}}>
+                <Container className="contt" >
+                    <h1 id="heading">IEEE MEC</h1>
                     <p id="para">
-                        This is a modified jumbotron that occupies the entire horizontal space of
                         its parent.
                     </p>
                 </Container>
-            </Jumbotron>
-        </div>
+    </Jumbotron>
+    
+     margin-top: 6.5rem;
+    height: calc( 100vh - 100px);
+    background-color: white;*/}
+    </div>
     )
 }
 
