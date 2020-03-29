39204394c04238643642972318b58facd557ac85
unknown <nikitamenon2510@gmail.com>
2020-03-29 11:13:48 +0530

    some fixes and jumbotron

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,17 +2,30 @@ import React from  'react';
 import './Title.css'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import main from "../main2.png"
+import heading from "../heading.png"
 
 const title = props =>{
     return(
         <div className="title">
-            <Jumbotron fluid className="jumbo">
+            <Jumbotron fluid className="jumbo" style={{'background-color':'white'}}>
                 <Container>
-                    <h1 id="heading">Fluid jumbotron</h1>
-                    <p id="para">
+                <div class="row">
+
+                    <div className="col s1">
+                        <img src={heading} alt="" className="heading_img"/>
+                        <p className="text">
                         This is a modified jumbotron that occupies the entire horizontal space of
+                        its parent. This is a modified jumbotron that occupies the entire horizontal space of
+                        its parent. This is a modified jumbotron that occupies the entire horizontal space of
+                        its parent. This is a modified jumbotron that occupies the entire horizontal space of
                         its parent.
-                    </p>
+                        </p>
+                    </div>
+                    <div className="col s1">
+                        <img src={main} alt="" className="main_img" />
+                    </div>
+                </div>
                 </Container>
             </Jumbotron>
         </div>
