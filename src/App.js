68daf3e1322342de61cceb852c939e365034362c
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-29 22:12:55 +0000

    code refactor

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React , { Component } from 'react';
 import Toolbar from './components/Toolbar/Toolbar'
 import Landing from './components/Landing.js'
 import './App.css';
+import Execom from './components/Execom/Execom.js'
+import Events from  './components/Events/Events.js'
+import Contacts from  './components/Contacts/Contacts.js'
+import Achievements from './components/Achievements/Achievements.js'
 import SideDrawer from './components/SideDrawer/SideDrawer.js'
-//import backdrop from './components/Backdrop/Backdrop.js'
+import {Router, Route}
 
 
 class App extends Component {
