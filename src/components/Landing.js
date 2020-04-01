ed5818d7ed92b4efd825782e2313c865390ab807
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 20:04:39 +0000

    working events and execom

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,6 +8,8 @@ import Footer from './Footer.js';
   import Missionvision from './Missionvision.js'
 import About from './About.js'
 import 'materialize-css/dist/css/materialize.min.css';
+import Execom from './Execom/Execom.js'
+import Events from './Events/Events.js'
 
 import Title from './Title/Title.js';
 
@@ -18,11 +20,12 @@ const Landing = props =>{
   return (
     <body>
     <div className="landing">
-      <div className="Title"><Title/></div>
+      <div><Events/></div>
+      {/*<div className="Title"><Title/></div>
       <div className="mission"><Missionvision/></div>
       <div className="about"><About/></div>
       <div className="gallery"><Photos/></div>
-      <div className="footer"><Footer/></div>
+  <div className="footer"><Footer/></div>*/}
     </div>
     </body>
   );
