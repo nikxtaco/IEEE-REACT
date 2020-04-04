d3300383774e15536e1da1a854848cd72a3b40f8
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-04 17:49:07 +0000

    changed event handler

--- src/components/EventHandler/EventHandler.js
+++ src/components/EventHandler/EventHandler.js
@@ -2,5 +2,37 @@ import React, { Component } from 'react';
 import events from "../Events/events.json"
 
 class EventHandler extends Component{
+    state={
+        ShowMainEvents: true
+      }
+      
+      drawerToggleClickHandler = () =>{
+        this.setState((prevState)=>{
+          return {ShowMainEvents: !prevState.ShowMainEvents}; // for opening the sidebar
+        });
+      };
+      closeToggleClickHandler= ()=>{
+        this.setState({SideDrawerOpen: false}); // for closing the sidebar
+      }
+      
+      
+      render(){
+        let adrawer;  
+      if(this.state.SideDrawerOpen){
+        adrawer=<SideDrawer closeClickHandler={this.closeToggleClickHandler}/>;
+      }
+      
+      return (
+        <div className="app">
+  
+         <Toolbar  drawerClickHandler={this.drawerToggleClickHandler}/>
+         
+         {adrawer}
+        
+        </div>
+      );
+      }
+    }
     
+    export default EventHandler;
 }
--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import 'materialize-css/dist/css/materialize.min.css';
+
 const NotFoundPage= () => {
       useEffect(() => {
       let parallel = document.querySelector('.parallax');
@@ -12,7 +13,9 @@ const NotFoundPage= () => {
     return (
       <div className="parallax-container hello">
         <div className="parallax"><img style={{"height": "100%", "width": "100%"}} className="responsive-img" src={"https://cdn.dribbble.com/users/2446071/screenshots/6903453/404.gif"} /></div>
-        <a className="waves-effect waves-light btn"><i className="material-icons left">arrow_back</i>Back</a>
+        <div className="container" id="nfb">
+        <a className="waves-effect waves-light deep-purple accent-3 btn notfoundbutton" id="g"><i className="material-icons left">arrow_back</i><Link to="/" style={{'color':'white'}} >Back</Link></a>
+        </div>
       </div>
     );
 }
